6dc5c16c69df423ef08a61e2371e51bb9c989e7e
Sagar Kumar <bittukumar9006716@gmail.com>
2026-03-29 16:15:00 +0530

    Update api.js

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api/v1', 
+    baseURL: 'https://backend-primetrade-ai.onrender.com/api/v1', 
     withCredentials: true, 
 });
 
